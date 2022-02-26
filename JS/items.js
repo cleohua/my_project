8d2a0b6b9659e2df5659fa7be410536fd0686e2c
cleohua <chadidi1218@gmail.com>
2022-02-26 12:35:48 +0800

    items加上圖片變換js

---
+++ JS/items.js
@@ -0,0 +1,26 @@
+// let img = picb.getElementsByTagName("img");
+// let BigGallery = document.getElementById("picX");
+// console.log(img)
+
+
+// for(i=0; i< img.length; i++){
+//     img[i].onclick = function(){
+//         BigGallery.src = this.src;
+//         return false;
+//     }
+
+// }
+
+
+// let img = picb.getElementsByTagName("img");
+// let BigGallery =document.getElementById("picX");
+// console.log(img)
+// for (i = 0; i < img.length; i++) {
+// img[i].onclick = function () {
+//     BigGallery.src = this.src;
+//     return false;
+//     }
+// }
+
+
+
